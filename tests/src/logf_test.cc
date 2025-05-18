8848bc675b5099cc2f6b765045167ded6b7714df
victorbaldin56 <victor.baldin.2005@gmail.com>
2025-05-18 18:31:52 +0000

    fix tests

--- tests/src/logf_test.cc
+++ tests/src/logf_test.cc
@@ -6,7 +6,9 @@
 
 namespace {
 
-constexpr unsigned kValuesPerSegment = 100;
+constexpr unsigned kValuesPerSegment = 1000000;
+static_assert((kValuesPerSegment & 0xf) == 0);
+
 constexpr double kMaxUlps = 3.5;
 
 float getUlp(float x) {
@@ -16,11 +18,11 @@ float getUlp(float x) {
 
 TEST(lalogf, ulp) {
   for (int e = -126; e < 127; ++e) {
-    double cur = std::pow(2.0, e);
-    double stride = cur / kValuesPerSegment;
+    float cur = std::pow(2.0, e);
+    float stride = cur / kValuesPerSegment;
     for (unsigned i = 0; i < kValuesPerSegment; i += 16) {
-      double val = cur + stride * i;
-      double ref = std::log(val);
+      float val = cur + stride * i;
+      double ref = std::log(static_cast<double>(val));
       float res = lalogf(val);
       float ulp = getUlp(res);
       double ulp_error =
@@ -32,27 +34,28 @@ TEST(lalogf, ulp) {
 
 TEST(lalogf_avx512, ulp) {
   for (int e = -126; e < 127; ++e) {
-    double cur = std::pow(2.0, e);
-    double stride = cur / kValuesPerSegment;
+    float cur = std::pow(2.f, static_cast<float>(e));
+    float stride = cur / kValuesPerSegment;
     for (unsigned i = 0; i < kValuesPerSegment; i += 16) {
-      std::array<double, 16> vals;
+      std::array<float, 16> vals;
       for (unsigned j = 0; j < 16; ++j) {
         vals[j] = cur + stride * (i + j);
       }
+
+      std::array<double, 16> double_vals;
+      std::transform(vals.begin(), vals.end(), double_vals.begin(),
+                     [](float v) { return static_cast<double>(v); });
       std::array<double, 16> res;
       std::transform(vals.begin(), vals.end(), res.begin(),
                      [](auto v) { return std::log(v); });
 
-      std::array<float, 16> float_vals;
-      std::transform(vals.begin(), vals.end(), float_vals.begin(),
-                     [](auto v) { return static_cast<float>(v); });
-      __m512 vec = _mm512_loadu_ps(float_vals.data());
+      __m512 vec = _mm512_loadu_ps(vals.data());
       __m512 res_vec = lalogf_avx512(vec);
       std::array<float, 16> res_vec_arr;
       _mm512_storeu_ps(res_vec_arr.data(), res_vec);
       std::array<double, 16> ulps;
       std::transform(res.begin(), res.end(), res_vec_arr.begin(), ulps.begin(),
-                     [](auto ref, auto val) {
+                     [](double ref, float val) {
                        float ulp = getUlp(val);
                        return std::abs(ref - static_cast<double>(val)) /
                               static_cast<double>(ulp);
