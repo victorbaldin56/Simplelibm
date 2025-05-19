beff075cd881731b708bcabe784d81458049f02a
victorbaldin56 <victor.baldin.2005@gmail.com>
2025-05-19 07:58:46 +0000

    100000 samples

--- tests/src/report.cc
+++ tests/src/report.cc
@@ -33,7 +33,7 @@ int main() try {
   logf_report << csv_header;
   logf_avx512_report << csv_header;
 
-  constexpr std::size_t kNumSamples = 10000;
+  constexpr std::size_t kNumSamples = 100000;
   constexpr float kMin = 0.0008f;
   constexpr float kMax = 8.f;
   constexpr float kStride = (kMax - kMin) / kNumSamples;
@@ -51,7 +51,7 @@ int main() try {
     std::memcpy(&bits_highp, &res_highp_f, 4);
     std::uint32_t diff = bits ^ bits_highp;
 
-    std::uint32_t first_bit = (diff ? 31 - __builtin_clz(diff) : -1);
+    std::uint32_t first_bit = (diff ? 32 - __builtin_clz(diff) : -1);
     logf_report << fmt::format("{:.8e},{:.8e},{:.8e},{:.8e},{}\n", x, res,
                                abs_error, rel_error, first_bit);
   }
@@ -100,7 +100,7 @@ int main() try {
 
     __mmask16 zero_m = _mm512_cmpeq_epi32_mask(diff, _mm512_set1_epi32(0));
     __m512i pos =
-        _mm512_sub_epi32(_mm512_set1_epi32(31), _mm512_lzcnt_epi32(diff));
+        _mm512_sub_epi32(_mm512_set1_epi32(32), _mm512_lzcnt_epi32(diff));
     __m512i first_bit =
         _mm512_mask_mov_epi32(pos, zero_m, _mm512_set1_epi32(-1));
     std::array<std::uint32_t, kElementsInVector> first_bits;
